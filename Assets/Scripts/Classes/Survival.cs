ab1c25c1ea97e5132fd59cec3e31e7be47dd9c96
Jaime Teixidó || MoONStter Teixi <MoONStterTeixi@gmail.com>
2018-05-17 19:59:21 +0200

    Algo

---
+++ Assets/Scripts/Classes/Survival.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Survival : MonoBehaviour {
+
+    private int vida { get; set; }
+    private int state { get; set; }
+    public Scrollbar VidaUI;
+    Animator animator;
+
+
+    public void Start()
+    {
+        animator = GetComponent<Animator>();
+        vida = DataClass.player.vidaMax;
+        StartCoroutine(EstateRequest());
+    }
+
+    public void Update()
+    {
+        VidaUI.size = vida / DataClass.player.vidaMax;
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag.Equals("Zombie"))
+        {
+            state = 1;
+        }
+    }
+
+    IEnumerator EstateRequest()
+    {
+        while (true)
+        {
+            switch (state)
+            {
+                case 0:
+
+                    break;
+                case 1:
+
+                    break;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Classes/Zombie.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombie : MonoBehaviour {
+
+    private int vida { get; set; }
+    private int damage { get; set; }
+    private int state { get; set; }
+    Animator animator;
+
+    public void Start()
+    {
+        animator = GetComponent<Animator>();
+        vida = 100;
+    }
+
+    public void Update()
+    {
+        if (vida <= 0)
+        {
+            this.animator.SetInteger("Status", 2);
+        }
+        else if(state == 1)
+        {
+            this.animator.SetInteger("Status", 1);
+        }
+        else
+        {
+            transform.Translate(new Vector2(-1, 0) * 2 * Time.deltaTime);
+
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag.Equals("Player"))
+        {
+            state = 1;
+        }
+        if (coll.gameObject.tag.Equals("Bala1"))
+        {
+            //gun
+        }
+        if (coll.gameObject.tag.Equals("Bala2"))
+        {
+            //torreta
+        }
+    }
+}
